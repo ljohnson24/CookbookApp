26fba1cee3b7a8e571261f509a7802d37d987cc7
Shaun <ljohnon24@luc.edu>
2018-11-30 18:56:48 -0600

    buggy

--- CookbookApp/Cookbook.DataAccess.SQL/Migrations/201811302352209_initial.Designer.cs
+++ CookbookApp/Cookbook.DataAccess.SQL/Migrations/201811302352209_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Cookbook.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class v11 : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(v11));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201811301017077_v1.1"; }
+            get { return "201811302352209_initial"; }
         }
         
         string IMigrationMetadata.Source
