3e535598709bf4030776e5ad1fdf9eb7590d0246
Shaun <ljohnon24@luc.edu>
2018-12-08 04:07:33 -0600

    updated

--- CookbookApp/Cookbook.DataAccess.SQL/Migrations/201812081004042_initial.Designer.cs
+++ CookbookApp/Cookbook.DataAccess.SQL/Migrations/201812081004042_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Cookbook.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class initial1 : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201812080035426_initial1"; }
+            get { return "201812081004042_initial"; }
         }
         
         string IMigrationMetadata.Source
